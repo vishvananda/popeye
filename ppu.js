476e533a274d4fa01f45b5f0fda3677793dc81fe
Vishvananda Abrams <vishvananda@gmail.com>
2021-11-10 13:18:52 -0600

    allow enable render in middle of line

--- ppu.js
+++ ppu.js
@@ -114,55 +114,54 @@ class PPU {
         let front = true;
         // update tile
         let [x, y] = [this.cycle - 1, this.scanline];
+        if (x % 8 == 0) {
+          let row = Math.floor(y / 8);
+          let column = x / 8;
+          let bank = (this.control & CR.BACKGROUND_PATTERN_ADDR) >> 4;
+          // get the proper nametable offset from the control register
+          let nt = (this.control & 0x03) << 10;
+          let offset = row * 32 + column;
+          let num = this.readVram(nt + offset);
+          let tile = this.cart.getTile(bank, num);
+          let finey = y % 8;
+          this.bgLo = tile[finey];
+          this.bgHi = tile[finey + 8];
+
+          let aoffset = Math.floor(row / 4) * 8 + Math.floor(column / 4);
+          let attr = this.readVram(nt + 0x3c0 + aoffset);
+          // shift attr right based on current quadrant
+          attr >>= ((row & 0x02) << 1) | (column & 0x02);
+          // explanation for bit math above
+          // rquad = row & 0x02 (== 0b10 if row is 3rd or 4th)
+          // cquad = col & 0x02 (== 0b10 if col is 3rd or 4th)
+          // bit pairs in table are low to high (attr is 0b BR BL TR TL)
+          // BITS QUAD ((rquad << 1) | cquad)
+          // ------------------------------
+          //   10  TL    0b000 | 0b000 = 0
+          //   32  TR    0b000 | 0b010 = 2
+          //   54  BL    0b100 | 0b000 = 4
+          //   76  BR    0b100 | 0b010 = 6
+          // eqivalent to
+          // if (row % 4 > 1) {
+          //   attr >>= 0b100;
+          // }
+          // if (column % 4 > 1) {
+          //   attr >>= 0b010;
+          // }
+
+          // use bottom two bytes to find palette index
+          let start = 0x01 + (attr & 0x03) * 4;
+          this.bgpal = [
+            null,
+            this.pal[this.palette[start]],
+            this.pal[this.palette[start + 1]],
+            this.pal[this.palette[start + 2]],
+          ];
+        }
         if (
           this.mask & Mask.BACKGROUND &&
           (right || this.mask & Mask.BACK_LEFT)
         ) {
-          if (x % 8 == 0) {
-            let row = Math.floor(y / 8);
-            let column = x / 8;
-            let bank = (this.control & CR.BACKGROUND_PATTERN_ADDR) >> 4;
-            // get the proper nametable offset from the control register
-            let nt = (this.control & 0x03) << 10;
-            let offset = row * 32 + column;
-            let num = this.readVram(nt + offset);
-            let tile = this.cart.getTile(bank, num);
-            let finey = y % 8;
-            this.bgLo = tile[finey];
-            this.bgHi = tile[finey + 8];
-
-            let aoffset = Math.floor(row / 4) * 8 + Math.floor(column / 4);
-            let attr = this.readVram(nt + 0x3c0 + aoffset);
-            // shift attr right based on current quadrant
-            attr >>= ((row & 0x02) << 1) | (column & 0x02);
-            // explanation for bit math above
-            // rquad = row & 0x02 (== 0b10 if row is 3rd or 4th)
-            // cquad = col & 0x02 (== 0b10 if col is 3rd or 4th)
-            // bit pairs in table are low to high (attr is 0b BR BL TR TL)
-            // BITS QUAD ((rquad << 1) | cquad)
-            // ------------------------------
-            //   10  TL    0b000 | 0b000 = 0
-            //   32  TR    0b000 | 0b010 = 2
-            //   54  BL    0b100 | 0b000 = 4
-            //   76  BR    0b100 | 0b010 = 6
-            // eqivalent to
-            // if (row % 4 > 1) {
-            //   attr >>= 0b100;
-            // }
-            // if (column % 4 > 1) {
-            //   attr >>= 0b010;
-            // }
-
-            // use bottom two bytes to find palette index
-            let start = 0x01 + (attr & 0x03) * 4;
-            this.bgpal = [
-              null,
-              this.pal[this.palette[start]],
-              this.pal[this.palette[start + 1]],
-              this.pal[this.palette[start + 2]],
-            ];
-          }
-
           // hi bit from msb of second plane
           let value = ((0x80 & this.bgHi) >> 6) | ((0x80 & this.bgLo) >> 7);
           // shift planes
@@ -462,7 +461,6 @@ class PPU {
         break;
       case 0x2004:
         // OAM Data
-        console.log("wrote oam data", this.oamAddr, data);
         this.oam[this.oamAddr] = data;
         this.oamAddr++;
         this.oamAddr &= 0xff;
